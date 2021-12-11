5b4519310b44caa4f3feb084a9ebbe179c44aedb
Raine Revere <raine@cybersemics.org>
2021-12-11 15:44:34 -0700

    Load and manipulate an SVG with p5.js-svg.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var svg, path
+function preload() {
+  svg = loadSVG('test.svg')
+  frameRate(20)
+}
+
 function setup() {
-  createCanvas(800, 800)
+  createCanvas(800, 800, SVG)
+  image(svg, 0, 0, 800, 800)
+  path = querySVG('path')[1]
 }
 
 function draw() {
-  background(220, 192, 200)
+  path.attribute('stroke-width', frameCount % 20)
 }
